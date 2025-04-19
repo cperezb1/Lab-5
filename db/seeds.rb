b3df04f91f8421fd1285c9275653c6089ff0f5c6
crist <crist@LAPTOP-O3VR4EOP.>
2025-04-18 21:21:57 -0400

    Lab 6 final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,16 @@
-#  10 usuarios con nombre y correo
 10.times do |i|
     User.create!(
-      email: "user#{i}@example.com",                # Puedes cambiar el formato del email
-      first_name: "Nombre#{i}",                     # Puedes cambiar el patrón de nombre
-      last_name: "Apellido#{i}"                     # Cambia el apellido o hazlo fijo si quieres
+      email: "user#{i}@example.com",                
+      first_name: "Nombre#{i}",                    
+      last_name: "Apellido#{i}"                    
     )
   end
+
   
-  # 10 chats entre usuarios aleatorios
   10.times do
     sender_id = User.pluck(:id).sample
     receiver_id = User.pluck(:id).sample
   
-    # Asegurarse de que sender y receiver no sean el mismo usuario
     while sender_id == receiver_id
       receiver_id = User.pluck(:id).sample
     end
@@ -23,12 +21,11 @@
     )
   end
   
-  # Crear 10 mensajes con texto genérico
   10.times do
     Message.create!(
-      chat_id: Chat.pluck(:id).sample,              # Puedes hacer que todos los mensajes vayan a un solo chat si quieres
-      user_id: User.pluck(:id).sample,              # Cambia esto si quieres que siempre escriba el mismo usuario
-      body: "Mensaje de prueba #{rand(1000)}" # Puedes variar el texto o usar lorem ipsum
+      chat_id: Chat.pluck(:id).sample,              
+      user_id: User.pluck(:id).sample,              
+      body: "Mensaje de prueba #{rand(1000)}" 
     )
   end
   
